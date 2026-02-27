92396cf842548b2ddf23c2080951eccb97de55dd
naum-27 <gjorshovski@proton.me>
2026-02-27 19:15:39 +0100

    Fix: Added CORS headers and OPTIONS handler

--- api/guess.js
+++ api/guess.js
@@ -8,8 +8,8 @@ export default async function handler(req, res) {
 
     // CORS Configuration
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.youtube.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
